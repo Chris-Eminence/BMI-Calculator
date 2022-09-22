ecdf7dd0ce9d691b1c18e6f7e1448a6859fadaaf
chris-eminence <emichris380@gmail.com>
2022-09-23 00:17:28 +0100

    reforctored code

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,6 @@
 import 'dart:ffi';
-
+import 'icon_content.dart';
+import 'reuseable_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -88,52 +89,4 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
-//iconWidget Extract
-class IconContent extends StatelessWidget {
-  IconContent({required this.icon, required this.label});
-
-  final IconData icon;
-  final String label;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(
-          icon,
-          size: 80.0,
-        ),
-        const SizedBox(
-          height: 10.0,
-        ),
-        Text(
-          label,
-          style: const TextStyle(
-            fontSize: 18.0,
-            color: Color(0xFF8D8E98),
-          ),
-        )
-      ],
-    );
-  }
-}
-
-class ReuseAbleCard extends StatelessWidget {
-  ReuseAbleCard({required this.colour, required this.cardChild});
-
-  final Color colour;
-  final Widget cardChild;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: colour,
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      child: cardChild,
-    );
-  }
-}
+//ic
