069db81e9157255395a91758435ca9f52af37e27
chris-eminence <emichris380@gmail.com>
2022-09-22 23:57:31 +0100

    adding icons

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,8 @@
+import 'dart:ffi';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const bottomContainerHeight = 70.0;
 const activeCardColor = Color(0xFF1D1E33);
@@ -25,32 +28,48 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: ReuseAbleCard(
                     colour: activeCardColor,
+                    cardChild:
+                        IconContent(icon: FontAwesomeIcons.mars, label: 'MALE'),
                   ),
                 ),
                 Expanded(
                   child: ReuseAbleCard(
                     colour: activeCardColor,
+                    cardChild: IconContent(
+                        icon: FontAwesomeIcons.venus, label: 'FEMALE'),
                   ),
                 ),
               ],
             ),
           ),
           Expanded(
-            child: ReuseAbleCard(
-              colour: activeCardColor,
+            child: Container(
+              margin: const EdgeInsets.all(15.0),
+              decoration: BoxDecoration(
+                color: activeCardColor,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
             ),
           ),
           Expanded(
             child: Row(
               children: [
                 Expanded(
-                  child: ReuseAbleCard(
-                    colour: activeCardColor,
+                  child: Container(
+                    margin: const EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                      color: activeCardColor,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
                   ),
                 ),
                 Expanded(
-                  child: ReuseAbleCard(
-                    colour: activeCardColor,
+                  child: Container(
+                    margin: const EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                      color: activeCardColor,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
                   ),
                 ),
               ],
@@ -69,10 +88,42 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
+//iconWidget Extract
+class IconContent extends StatelessWidget {
+  IconContent({required this.icon, required this.label});
+
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          size: 80.0,
+        ),
+        const SizedBox(
+          height: 10.0,
+        ),
+        Text(
+          label,
+          style: const TextStyle(
+            fontSize: 18.0,
+            color: Color(0xFF8D8E98),
+          ),
+        )
+      ],
+    );
+  }
+}
+
 class ReuseAbleCard extends StatelessWidget {
-  ReuseAbleCard({required this.colour});
+  ReuseAbleCard({required this.colour, required this.cardChild});
 
   final Color colour;
+  final Widget cardChild;
 
   @override
   Widget build(BuildContext context) {
@@ -82,6 +133,7 @@ class ReuseAbleCard extends StatelessWidget {
         color: colour,
         borderRadius: BorderRadius.circular(10.0),
       ),
+      child: cardChild,
     );
   }
 }
